a0463c0c5e5b34b2e2aafd18fb4d821d78dfbc8d
jaysinco <jaysinco@163.com>
2019-09-21 15:54:35 +0800

    add imgui

---
+++ common/utility/hint_mouse_functest.cc
@@ -0,0 +1,129 @@
+#include <Windows.h>
+#include <string>
+#include <sstream>
+#include <thread>
+
+#include "common/utility/sysinfo_utility_win.h"
+
+#define RL(x) ((double)x * GetSystemMetrics(SM_CXFULLSCREEN) * gDPI)
+
+double gDPI = WinInfoUtility::GetDPI() / 96;
+HWND gWindow = NULL;
+DWORD gMessageThreadId = -1;
+HHOOK gMouseHook = NULL;
+
+struct InfoBoard
+{
+    std::string mousePos;
+} gBoard;
+
+HWND createHintWindow(const std::string &classname, WNDPROC windowProc,
+                        const std::string &color = "#FFFFFF", double alpha = 1.0f,
+                        int width = CW_USEDEFAULT, int height = CW_USEDEFAULT,
+                        int xPos = CW_USEDEFAULT, int yPos = CW_USEDEFAULT)
+{
+    WNDCLASS wc = {0};
+    const char *clsname = classname.c_str();
+    wc.lpszClassName = clsname;
+    wc.lpfnWndProc = windowProc;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    DWORD r, g, b;
+    sscanf_s(color.c_str(), "#%2X%2X%2X", &r, &g, &b);
+    wc.hbrBackground = CreateSolidBrush(RGB(r, g, b));
+    UnregisterClass(clsname, GetModuleHandle(NULL));
+    RegisterClass(&wc);
+
+    HWND handle = CreateWindowEx(
+        WS_EX_TOOLWINDOW | WS_EX_LAYERED, // Optional window styles.
+        clsname,                          // Window class
+        "",                               // Window caption
+        WS_POPUP,                         // Window style
+        xPos,                             // Postion x
+        yPos,                             // Postion y
+        width,                            // Width
+        height,                           // Height
+        NULL,                             // Parent window
+        NULL,                             // Menu
+        GetModuleHandle(NULL),            // Instance handle
+        NULL                              // Additional application data
+    );
+    SetLayeredWindowAttributes(handle, NULL, 255 * alpha, LWA_ALPHA);
+    ShowWindow(handle, SW_HIDE);
+    return handle;
+}
+
+LRESULT CALLBACK textWindowProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_PAINT:
+    {
+        HFONT hFont;
+        hFont = CreateFontW(
+            RL(0.008),             // height
+            0,                     // width
+            0,                     // escapenment,
+            0,                     // orientation,
+            0,                     // weight,
+            FALSE,                 // italic,
+            FALSE,                 // underline,
+            FALSE,                 // strikeOut,
+            DEFAULT_CHARSET,       // charset,
+            OUT_CHARACTER_PRECIS,  // outPrecision,
+            CLIP_CHARACTER_PRECIS, // clipPrecision,
+            DEFAULT_QUALITY,       // quality,
+            FF_DONTCARE,           // pitchAndFamily,
+            L"华文细黑"             // facename
+        );
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
+        HFONT oldFont = (HFONT)SelectObject(hdc, hFont);
+        TextOut(hdc, RL(0.0015), RL(0.0015), gBoard.mousePos.c_str(), gBoard.mousePos.size());
+        SelectObject(hdc, oldFont);
+        EndPaint(hwnd, &ps);
+        return 0;
+    }
+    }
+    return DefWindowProc(hwnd, message, wParam, lParam);
+}
+
+LRESULT CALLBACK mouseProc(INT nCode, WPARAM wParam, LPARAM lParam)
+{
+    PMSLLHOOKSTRUCT p = (PMSLLHOOKSTRUCT)lParam;
+    if (!(nCode < 0 || p->flags) && wParam == WM_MOUSEMOVE)
+    {
+        int x = p->pt.x / gDPI + RL(0.005);
+        int y = p->pt.y / gDPI + RL(0.008);
+        std::ostringstream ss;
+        ss << p->pt.x << "x" << p->pt.y;
+        gBoard.mousePos = ss.str();
+        InvalidateRect(gWindow, NULL, TRUE);
+        SetWindowPos(gWindow, HWND_TOPMOST, x, y, 0, 0, SWP_SHOWWINDOW | SWP_NOSIZE);
+    }
+    return CallNextHookEx(gMouseHook, nCode, wParam, lParam);
+}
+
+void createMessageLoop()
+{
+    gMessageThreadId = GetCurrentThreadId();
+    gMouseHook = SetWindowsHookEx(WH_MOUSE_LL, mouseProc, GetModuleHandle(NULL), 0);
+
+    gWindow = createHintWindow("hintWindow", textWindowProc, "#FFFFFF", 0.8, RL(0.038), RL(0.01));
+    MSG msg;
+
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    UnhookWindowsHookEx(gMouseHook);
+}
+
+int main()
+{
+    std::thread t(createMessageLoop);
+    // Sleep(3000);
+    // PostThreadMessage(gMessageThreadId, WM_QUIT, 0, NULL);
+    t.join();
+}
--- product/testbed/testbed.cc
+++ product/testbed/testbed.cc
@@ -1,133 +1,146 @@
-#include <Windows.h>
-#include <string>
-#include <sstream>
-#include <thread>
 #define GLOG_NO_ABBREVIATED_SEVERITIES
 #define GOOGLE_GLOG_DLL_DECL
 #include <glog/logging.h>
-#include <gtest/gtest.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
-#include "common/utility/sysinfo_utility_win.h"
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 
-#define RL(x) ((double)x * GetSystemMetrics(SM_CXFULLSCREEN) * gDPI)
-
-double gDPI = WinInfoUtility::GetDPI() / 96;
-HWND gWindow = NULL;
-DWORD gMessageThreadId = -1;
-HHOOK gMouseHook = NULL;
-
-struct InfoBoard
-{
-    std::string mousePos;
-} gBoard;
-
-HWND createSimpleWindow(const std::string &classname, WNDPROC windowProc,
-                        const std::string &color = "#FFFFFF", double alpha = 1.0f,
-                        int width = CW_USEDEFAULT, int height = CW_USEDEFAULT,
-                        int xPos = CW_USEDEFAULT, int yPos = CW_USEDEFAULT)
-{
-    WNDCLASS wc = {0};
-    const char *clsname = classname.c_str();
-    wc.lpszClassName = clsname;
-    wc.lpfnWndProc = windowProc;
-    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-    DWORD r, g, b;
-    sscanf_s(color.c_str(), "#%2X%2X%2X", &r, &g, &b);
-    wc.hbrBackground = CreateSolidBrush(RGB(r, g, b));
-    UnregisterClass(clsname, GetModuleHandle(NULL));
-    RegisterClass(&wc);
-
-    HWND handle = CreateWindowEx(
-        WS_EX_TOOLWINDOW | WS_EX_LAYERED, // Optional window styles.
-        clsname,                          // Window class
-        "",                               // Window caption
-        WS_POPUP,                         // Window style
-        xPos,                             // Postion x
-        yPos,                             // Postion y
-        width,                            // Width
-        height,                           // Height
-        NULL,                             // Parent window
-        NULL,                             // Menu
-        GetModuleHandle(NULL),            // Instance handle
-        NULL                              // Additional application data
-    );
-    SetLayeredWindowAttributes(handle, NULL, 255 * alpha, LWA_ALPHA);
-    ShowWindow(handle, SW_HIDE);
-    return handle;
+static void glfw_error_callback(int error, const char* description) {
+    LOG(ERROR) << "Glfw Error<" << error << ">: " << description;
 }
 
-LRESULT CALLBACK textWindowProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    switch (message)
+int main(int argc, char *argv[]) {
+	google::InitGoogleLogging(argv[0]);
+	FLAGS_logtostderr = 1;
+	FLAGS_minloglevel = 0;
+	LOG(INFO) << "start testbed...";
+
+	// Setup window
+    glfwSetErrorCallback(glfw_error_callback);
+	glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Dear ImGui GLFW+OpenGL3 example", NULL, NULL);
+	if (window == NULL) {
+		LOG(ERROR) << "Failed to create GLFW window";
+		glfwTerminate();
+		return -1;
+	}
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // Enable vsync
+
+	// Initialize OpenGL loader
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		LOG(ERROR) << "Failed to initialize GLAD";
+		return -1;
+	}
+
+	// Setup ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    ImGui::StyleColorsDark();
+    //ImGui::StyleColorsClassic();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 130");
+
+    // Load Fonts
+    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
+    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
+    // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
+    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
+    // - Read 'misc/fonts/README.txt' for more instructions and details.
+    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
+    //io.Fonts->AddFontDefault();
+    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
+    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
+    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
+    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/ProggyTiny.ttf", 10.0f);
+    //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
+    //IM_ASSERT(font != NULL);
+
+    // Our state
+    bool show_demo_window = true;
+    bool show_another_window = false;
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+    // Main loop
+    while (!glfwWindowShouldClose(window))
     {
-    case WM_PAINT:
-    {
-        HFONT hFont;
-        hFont = CreateFontW(
-            RL(0.014),             // height
-            0,                     // width
-            0,                     // escapenment,
-            0,                     // orientation,
-            0,                     // weight,
-            FALSE,                 // italic,
-            FALSE,                 // underline,
-            FALSE,                 // strikeOut,
-            DEFAULT_CHARSET,       // charset,
-            OUT_CHARACTER_PRECIS,  // outPrecision,
-            CLIP_CHARACTER_PRECIS, // clipPrecision,
-            DEFAULT_QUALITY,       // quality,
-            FF_DONTCARE,           // pitchAndFamily,
-            L"黑体"                // facename
-        );
-        PAINTSTRUCT ps;
-        HDC hdc = BeginPaint(hwnd, &ps);
-        HFONT oldFont = (HFONT)SelectObject(hdc, hFont);
-        TextOut(hdc, RL(0.003), RL(0.003), gBoard.mousePos.c_str(), gBoard.mousePos.size());
-        SelectObject(hdc, oldFont);
-        EndPaint(hwnd, &ps);
-        return 0;
+        // Poll and handle events (inputs, window resize, etc.)
+        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
+        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
+        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
+        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
+        glfwPollEvents();
+
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+        if (show_demo_window)
+            ImGui::ShowDemoWindow(&show_demo_window);
+
+        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+
+            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+            ImGui::Checkbox("Another Window", &show_another_window);
+
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+                counter++;
+            ImGui::SameLine();
+            ImGui::Text("counter = %d", counter);
+
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+            ImGui::End();
+        }
+
+        // 3. Show another simple window.
+        if (show_another_window)
+        {
+            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+            ImGui::Text("Hello from another window!");
+            if (ImGui::Button("Close Me"))
+                show_another_window = false;
+            ImGui::End();
+        }
+
+        // Rendering
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
     }
-    }
-    return DefWindowProc(hwnd, message, wParam, lParam);
-}
 
-LRESULT CALLBACK mouseProc(INT nCode, WPARAM wParam, LPARAM lParam)
-{
-    PMSLLHOOKSTRUCT p = (PMSLLHOOKSTRUCT)lParam;
-    if (!(nCode < 0 || p->flags) && wParam == WM_MOUSEMOVE)
-    {
-        int x = p->pt.x / gDPI + RL(0.005);
-        int y = p->pt.y / gDPI + RL(0.008);
-        std::ostringstream ss;
-        ss << p->pt.x << "x" << p->pt.y;
-        gBoard.mousePos = ss.str();
-        InvalidateRect(gWindow, NULL, TRUE);
-        SetWindowPos(gWindow, HWND_TOPMOST, x, y, 0, 0, SWP_SHOWWINDOW | SWP_NOSIZE);
-    }
-    return CallNextHookEx(gMouseHook, nCode, wParam, lParam);
-}
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
 
-void createMessageLoop()
-{
-    gMessageThreadId = GetCurrentThreadId();
-    gMouseHook = SetWindowsHookEx(WH_MOUSE_LL, mouseProc, GetModuleHandle(NULL), 0);
+    glfwDestroyWindow(window);
+    glfwTerminate();
 
-    gWindow = createSimpleWindow("hintWindow", textWindowProc, "#FFFFFF", 0.8, RL(0.07), RL(0.02));
-    MSG msg;
-
-    while (GetMessage(&msg, NULL, 0, 0))
-    {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
-
-    UnhookWindowsHookEx(gMouseHook);
-}
-
-int main()
-{
-    std::thread thread(createMessageLoop);
-    // Sleep(3000);
-    // PostThreadMessage(gMessageThreadId, WM_QUIT, 0, NULL);
-    thread.join();
+    return 0;
 }
