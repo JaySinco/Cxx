9fa538d96a8a915d578b9a95e39b5599d8e54e6c
jaysinco <jaysinco@163.com>
2020-03-05 23:29:22 +0800

    use mkldnn for mxnet

--- external/mxnet/build.sh
+++ external/mxnet/build.sh
@@ -7,17 +7,15 @@ mkdir -p build
 cd build
 MKL_DLL_PATH="/c/Program Files (x86)/IntelSWTools/compilers_and_libraries_2020.0.166/windows/redist/intel64_win/mkl"
 mkdir -p Release
-cp "${MKL_DLL_PATH}/mkl_rt.dll" Release
-cmake -G "Visual Studio 15 2017 Win64" -T "host=x64" ../ \
+# 编译过程中会报错一次，需要手动将编译好的mkldnn.dll放到Release中
+cmake -G "Visual Studio 15 2017 Win64" -T "host=x64" ../apache-mxnet-src-1.6.0-incubating/ \
     -DUSE_CUDA=0 -DUSE_CUDNN=0 -DUSE_NVRTC=0 -DUSE_OPENCV=1 \
     -DBUILD_CPP_EXAMPLES=0 -DBUILD_TESTING=0 \
     -DUSE_OPENMP=1 -DUSE_PROFILER=1 -DUSE_BLAS=mkl \
     -DUSE_LAPACK=1 -DUSE_DIST_KVSTORE=0 -DCUDA_ARCH_NAME=All \
-    -DUSE_MKLDNN=0 -DCMAKE_BUILD_TYPE=Release \
+    -DUSE_MKLDNN=1 -DCMAKE_BUILD_TYPE=Release \
     -DMKL_ROOT="C:\Program Files (x86)\IntelSWTools\compilers_and_libraries\windows\mkl" \
-    -DOpenCV_DIR=`readlink -f ../../../opencv/opencv-3.4.9/build` \
+    -DOpenCV_DIR=`readlink -f ../../opencv/opencv-3.4.9/build` \
     -DUSE_CPP_PACKAGE=1 -DCMAKE_INSTALL_PREFIX="./mxnet" && \
 MSBuild.exe -p:Configuration=Release *.sln -maxcpucount $@ && \
 MSBuild.exe INSTALL.vcxproj -p:Configuration=Release
-cp "${MKL_DLL_PATH}/mkl_rt.dll" "./mxnet/bin"
-cp "${MKL_DLL_PATH}/mkl_intel_thread.dll" "./mxnet/bin"
