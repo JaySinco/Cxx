63d9062784b662c3aebf5cdbb4cb3d66af0f4c16
jaysinco <jaysinco@163.com>
2020-03-08 20:15:45 +0800

    add demo lua

--- product/testbed/lua.cc
+++ product/testbed/lua.cc
@@ -1,10 +1,24 @@
 #include "common/utility/logging.h"
+#include "common/utility/string_helper.h"
+#include <fstream>
 #include <iostream>
 #include <lua.hpp>
+#include <sstream>
+
+using namespace cxx;
 
 static int l_log(lua_State* L)
 {
-    LOG(INFO) << lua_tostring(L, 1);
+    std::ostringstream ss;
+    int top = lua_gettop(L);
+    for (int i = 1; i <= top; i++) {
+        if (i > 1)
+            ss << " ";
+        std::string str(lua_tostring(L, i));
+        ss << encodeAnsi(decodeUtf8(str));
+    }
+
+    LOG(INFO) << ss.str();
     lua_pushnil(L);
     return 1;
 }
@@ -15,18 +29,24 @@ int main(int argc, char* argv[])
     gflags::ParseCommandLineFlags(&argc, &argv, true);
     FLAGS_logtostderr = 1;
     FLAGS_minloglevel = 0;
+    std::string source = "D:\\Jaysinco\\Cxx\\product\\testbed\\resources\\demo.lua";
+    if (argc > 1)
+        source = argv[1];
+    std::ifstream file(source);
+    if (!file) {
+        LOG(ERROR) << "failed to open source file: " << source;
+        exit(0);
+    }
+    std::string code(
+        std::istreambuf_iterator<char> { file },
+        std::istreambuf_iterator<char> {});
     lua_State* L = luaL_newstate();
     luaL_openlibs(L);
     lua_pushcfunction(L, l_log);
     lua_setglobal(L, "log");
-    std::string cmd;
-    std::cout << ">>> " << std::flush;
-    while (std::getline(std::cin, cmd)) {
-        if (luaL_loadstring(L, cmd.c_str()) || lua_pcall(L, 0, 0, 0)) {
-            std::cerr << lua_tostring(L, -1) << std::endl;
-            lua_pop(L, 1);
-        }
-        std::cout << ">>> " << std::flush;
+    if (luaL_loadstring(L, code.c_str()) || lua_pcall(L, 0, 0, 0)) {
+        std::cerr << lua_tostring(L, -1) << std::endl;
+        lua_pop(L, 1);
     }
     lua_close(L);
 }
