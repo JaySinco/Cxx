2c4044fd78a8a4d14b1674e0e545d51570416521
jaysinco <jaysinco@163.com>
2020-03-21 00:32:06 +0800

    fix boost use static runtime bug

--- external/boost/build.sh
+++ external/boost/build.sh
@@ -1,7 +1,7 @@
 # need to pass -DBoost_COMPILER="-vc141" to CMake to make it work
 
 ./b2.exe --with-system --with-thread --with-filesystem --with-chrono\
-    --with-program_options --with-test\
+    --with-program_options --with-test --with-timer \
     toolset=msvc variant=release link=static \
     architecture=x86 address-model=64 threading=multi \
     runtime-link=shared
--- external/quantlib/build.sh
+++ external/quantlib/build.sh
@@ -1,29 +1,17 @@
 #!/bin/bash
 
-_dir=`dirname $(readlink -f $0)`
 MSVC_PATH="/c/Program Files (x86)/Microsoft Visual Studio/2017/Community/VC/Tools/MSVC/14.16.27023/bin/HostX86/x86:/c/Program Files (x86)/Microsoft Visual Studio/2017/Community/Common7/IDE/VC/VCPackages:/c/Program Files (x86)/Microsoft SDKs/TypeScript/3.1:/c/Program Files (x86)/Microsoft Visual Studio/2017/Community/Common7/IDE/CommonExtensions/Microsoft/TestWindow:/c/Program Files (x86)/Microsoft Visual Studio/2017/Community/Common7/IDE/CommonExtensions/Microsoft/TeamFoundation/Team Explorer:/c/Program Files (x86)/Microsoft Visual Studio/2017/Community/MSBuild/15.0/bin/Roslyn:/c/Program Files (x86)/Microsoft Visual Studio/2017/Community/Team Tools/Performance Tools:/c/Program Files (x86)/Microsoft Visual Studio/Shared/Common/VSPerfCollectionTools/:/c/Program Files (x86)/Microsoft SDKs/Windows/v10.0A/bin/NETFX 4.6.1 Tools/:/c/Program Files (x86)/Windows Kits/10/bin/10.0.17763.0/x86:/c/Program Files (x86)/Windows Kits/10/bin/x86:/c/Program Files (x86)/Microsoft Visual Studio/2017/Community//MSBuild/15.0/bin:/c/Windows/Microsoft.NET/Framework/v4.0.30319:/c/Program Files (x86)/Microsoft Visual Studio/2017/Community/Common7/IDE/:/c/Program Files (x86)/Microsoft Visual Studio/2017/Community/Common7/Tools/"
 PATH="${MSVC_PATH};${PATH}"
-
-cmake -G "Visual Studio 15 2017 Win64" -T "host=x64"  ../ \
--DLIBEVENT_ROOT=`readlink -f ../../../windows/x64/libevent/` \
--DLIBEVENT_INCLUDE_DIRS=`readlink -f ../../../windows/x64/libevent/include/` \
--DLIBEVENT_LIBRARIES=`readlink -f ../../../windows/x64/libevent/lib/` \
--DOPENSSL_ROOT_DIR=`readlink -f ../../../windows/x64/openssl/` \
--DOPENSSL_CRYPTO_LIBRARY=`readlink -f ../../../windows/x64/openssl/lib/` \
--DOPENSSL_INCLUDE_DIR=`readlink -f ../../../windows/x64/openssl/include/` \
--DBOOST_ROOT=`readlink -f ../../../windows/x64/boost/` \
--DBOOST_LIBRARYDIR=`readlink -f ../../../windows/x64/boost/lib/` \
--DBoost_USE_STATIC_RUNTIME=off \
--DBoost_COMPILER="-vc141" \
--DZLIB_ROOT=`readlink -f ../../../zlib/zlib-1.2.11/` \
--DCMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE=$_dir/bin/ \
--DWITH_SHARED_LIB=off \
--DWITH_STATIC_LIB=on \
--DWITH_STDTHREADS=on \
--DWITH_BOOSTTHREADS=off \
--DWITH_BOOST_STATIC=on \
--DWITH_ZLIB=on \
--DWITH_LIBEVENT=on \
--DBUILD_TESTING=off \
--DMSVC_RUNTIME=dynamic && \
-MSBuild.exe -p:Configuration=Release *.sln $@
+mkdir -p build
+cd build
+cmake -G "Visual Studio 15 2017 Win64" -T "host=x64" ../QuantLib-1.17/ \
+    -DBOOST_ROOT=`readlink -f ../../windows/x64/boost/` \
+    -DBOOST_LIBRARYDIR=`readlink -f ../../windows/x64/boost/lib/` \
+    -DBoost_INCLUDE_DIR=`readlink -f ../../windows/x64/boost/include/` \
+    -DBoost_USE_STATIC_RUNTIME=off \
+    -DBoost_COMPILER="-vc141" \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX="./QuantLib" \
+    -DMSVC_RUNTIME=dynamic && \
+MSBuild.exe -p:Configuration=Release QuantLib.sln -maxcpucount $@ && \
+MSBuild.exe INSTALL.vcxproj -p:Configuration=Release
