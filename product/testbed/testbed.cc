b19afde78a46a4ff887b744162663c5174acc47f
jaysinco <jaysinco@163.com>
2020-06-13 20:56:10 +0800

    add testbed

---
+++ product/testbed/testbed.cc
@@ -0,0 +1,9 @@
+#include "common/utility/base.h"
+
+int main(int argc, char* argv[])
+{
+    google::InitGoogleLogging(argv[0]);
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
+    FLAGS_logtostderr = 1;
+    FLAGS_minloglevel = 0;
+}
